1fbe5100c381fe498662fc7e913f52e756e12f08
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-10-20 11:19:30 +0200

    Prepare for 0.06 version

--- Data.pm
+++ Data.pm
@@ -8,7 +8,7 @@ use English;
 use Error::Pure qw(err);
 use Perl6::Slurp qw(slurp);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 sub new {
 	my ($class, @params) = @_;
@@ -268,6 +268,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
